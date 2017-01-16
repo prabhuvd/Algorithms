a452bd7c814486a86048011191d1e34a4a65a09f
pdesai <prabhu.desai@gmail.com>
2017-01-15 20:30:54 -0500

    Fixed the union function.

--- Algorithms/percolation/QUnionImprove.py
+++ Algorithms/percolation/QUnionImprove.py
@@ -18,7 +18,7 @@ class QUnionImprove:
         self.__quimpNodes=[]
         self.__quiBlockedNodes=[]  
         self.__quiNodeSize=[]        
-        for arrIndex in range((row_columns*row_columns)+1):    
+        for arrIndex in range((row_columns*row_columns)):    
             self.__quimpNodes.append(arrIndex)
             self.__quiBlockedNodes.append(0)   
             self.__quiNodeSize.append(1)     
@@ -44,7 +44,7 @@ class QUnionImprove:
             return False        
     
     def union(self,node_a,node_b):
-        #print "Union (",node_a,node_b,")"
+        print "Union (",node_a,node_b,")"
         root_of_node_a = self.root(node_a)
         root_of_node_b = self.root(node_b)
         
@@ -53,37 +53,44 @@ class QUnionImprove:
         if(root_of_node_a == root_of_node_b):
             return
         
-        if(self.__quiNodeSize[node_a]<self.__quiNodeSize[node_b]):
+        if(self.__quiNodeSize[root_of_node_a]<self.__quiNodeSize[root_of_node_b]):
             self.__quimpNodes[root_of_node_a] = root_of_node_b
-            self.__quiNodeSize[root_of_node_b] =self.__quiNodeSize[root_of_node_b]+1
+            self.__quiNodeSize[root_of_node_b] =self.__quiNodeSize[root_of_node_b]+self.__quiNodeSize[node_a]
              
         else:
             self.__quimpNodes[root_of_node_b] = root_of_node_a
-            self.__quiNodeSize[root_of_node_a] =self.__quiNodeSize[root_of_node_a]+1 
+            self.__quiNodeSize[root_of_node_a] =self.__quiNodeSize[root_of_node_a]+self.__quiNodeSize[root_of_node_b]
             
-#         print self.__quimpNodes   
-#         print self.__quiNodeSize
+        print self.__quimpNodes   
+        print self.__quiNodeSize
 
         
-# x = QUnionImprove(3)
-#  
-#  
-# x.union(4,3)
-#  
-# print x.connected(3, 4)
-# print x.root(3)
-# print x.root(4)
-#  
-# x.union(3,8)
+x = QUnionImprove(3)
+  
+  
+x.union(4,3)
+  
+print x.connected(3, 4)
+print x.root(3)
+print x.root(4)
+  
+x.union(3,8)
+
+
+x.union(1,5)
+x.union(2,5)
+
+x.union(4,5)
+
 # x.union(6,5)
 # x.union(9,4)
 # x.union(2,1)
-#   
-#  
+   
+  
 # print x.connected(1,7)
-#  
-# x.union(5,0)
-# x.union(7,2)
-# x.union(6,1)
-# x.union(7,3)
+  
+x.union(5,0)
+x.union(7,2)
+x.union(6,1)
+x.union(7,3)
 
